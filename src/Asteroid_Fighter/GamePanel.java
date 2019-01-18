c302c9a63923b8653fb4b23e7cdd4e53482a9aab
seth <sethtinglof@gmail.com>
2019-01-17 23:48:16 -0500

    cleaned up import

--- src/Asteroid_Fighter/GamePanel.java
+++ src/Asteroid_Fighter/GamePanel.java
@@ -8,7 +8,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class GamePanel extends JPanel{
